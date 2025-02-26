c0c5cb0acb1dc7c10eee3e7e1200aef7379e124b
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-26 21:49:23 +0900

    💄 Design: 메인페이지 백그라운드 비디오 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h1>홈페이지</h1>
-    </div>
+    <section>
+      <div className="main ">
+        <div className="main-video-back">
+          <video className="main-video" autoPlay loop muted playsInline>
+            <source src="https://test.chalix.co.kr/images/main-video-section/mainvid.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <h1>홈페이지</h1>
+      </div>
+    </section>
   );
 }
