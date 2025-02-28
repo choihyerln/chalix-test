a20cef64f032b9df2d68fea019bbbba42c8a968d
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-28 18:50:32 +0900

    ✨ Feat: `ScrollToTopButton` 기능 구현

--- src/components/ScrollToTopBtn.tsx
+++ src/components/ScrollToTopBtn.tsx
@@ -1,13 +1,19 @@
 import "@/styles/ScrollToTopBtn.css";
 
 export default function ScrollToTopBtn() {
+  const handleTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
   return (
     <section className="flexd-top-section">
-      <div className="cover">
+      <button className="cover" onClick={handleTop}>
         <svg
           stroke="#000"
           fill="#000"
-          stroke-width="0"
+          strokeWidth="0"
           viewBox="0 0 24 24"
           height="1em"
           width="1em"
@@ -15,7 +21,7 @@ export default function ScrollToTopBtn() {
         >
           <path d="M18.655 10.405a.75.75 0 0 1-1.06 0l-4.97-4.97v14.44a.75.75 0 0 1-1.5 0V5.435l-4.97 4.97a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734l6.25-6.25a.75.75 0 0 1 1.06 0l6.25 6.25a.75.75 0 0 1 0 1.06Z"></path>
         </svg>
-      </div>
+      </button>
     </section>
   );
 }
