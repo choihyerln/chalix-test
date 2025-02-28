4c478f96a0477ec3accece2ff00e09264b61e0c6
choihyerln <choi.hyehyehihi@gmail.com>
2025-02-28 18:19:14 +0900

    ✨ Feat: `ScrollToTopButton` UI 추가

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/layout/Header";
+import ScrollToTopBtn from "@/components/ScrollToTopBtn";
 
 export default function MainLayout() {
   return (
     <>
       <Header />
+      <ScrollToTopBtn />
       <main>
         <Outlet />
       </main>
